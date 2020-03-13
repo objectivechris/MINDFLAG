b3a3c643ff9ba7ed6458d5f02da0809848b1664c
Christopher Rene <roderick540@gmail.com>
2020-03-13 11:02:00 -0400

    Initial Commit

---
+++ MINDFLAG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MINDFLAG
+//
+//  Created by Christopher Rene on 3/13/20.
+//  Copyright © 2020 Christopher Rene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
